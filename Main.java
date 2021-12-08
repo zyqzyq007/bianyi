7b28bc95b9bbfd95b10b7ba05129d08aa65cb796
ZYQ <593458187@qq.com>
2021-12-08 19:07:53 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+public class Main {
+public static void main(String[] args)  {
+
+        CharStream input = CharStreams.fromFileName(args[0]);
+       
+        System.out.println(input);
+        //System.exit(231);
+
+
+    }
+}
